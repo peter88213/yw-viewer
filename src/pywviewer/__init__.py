3db3a7c7385b62dc54fc9292070ff9fba662b814
peter88213 <peter.88213@t-online.de>
2022-01-19 10:37:37 +0100

    Add docstring to the pywviewer package

---
+++ src/pywviewer/__init__.py
@@ -0,0 +1,10 @@
+"""Package for yWriter file viewer.
+
+Modules:
+
+file-viewer -- Provide a class for yWriter file viewing.
+
+Copyright (c) 2022 Peter Triesberger
+For further information see https://github.com/peter88213/yw-viewer
+Published under the MIT License (https://opensource.org/licenses/mit-license.php)
+"""
