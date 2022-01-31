7fbb10504f09615849c7bedd560636b9b7949577
peter88213 <peter.88213@t-online.de>
2022-01-31 12:55:43 +0100

    Refactor build and setup scripts.

--- src/build_yw_viewer.py
+++ src/build_yw_viewer.py
@@ -15,7 +15,7 @@ import inliner
 SRC = '../src/'
 BUILD = '../test/'
 SOURCE_FILE_GUI = 'yw_viewer_.pyw'
-TARGET_FILE_GUI = BUILD + 'yw_viewer.pyw'
+TARGET_FILE_GUI = f'{BUILD}yw_viewer.pyw'
 
 
 def main():
