c5307384704c872fd8c53a484208218fc4c6187a
peter88213 <peter.88213@t-online.de>
2022-02-03 14:58:59 +0100

    Prepare v 1.3.4 based on PyWriter v4.1.11
    Refactor
    - Make methods and instance variables "private" if not part of the
    public interface.

--- docs/index.md
+++ docs/index.md
@@ -25,9 +25,9 @@ The *yw-viewer* script is mainly intended for Linux users who manage multiple yW
 
 ## Download and install
 
-[Download the latest release (version 1.3.3)](https://raw.githubusercontent.com/peter88213/yw-viewer/main/dist/yw-viewer_v1.3.3.zip)
+[Download the latest release (version 1.3.4)](https://raw.githubusercontent.com/peter88213/yw-viewer/main/dist/yw-viewer_v1.3.4.zip)
 
-- Unzip the downloaded zipfile "yw-viewer_v1.3.3.zip" into a new folder.
+- Unzip the downloaded zipfile "yw-viewer_v1.3.4.zip" into a new folder.
 - Move into this new folder and launch **setup.pyw**. This installs the script for the local user.
 - Create a shortcut on the desktop when asked.
 - Open "README.md" for usage instructions.
--- src/pywviewer/yw7_viewer_tk.py
+++ src/pywviewer/yw7_viewer_tk.py
@@ -27,58 +27,58 @@ class Yw7ViewerTk(MainTk):
         Extend the superclass constructor.
         """
         super().__init__(title, **kwargs)
-        self.textBox = RichTextTk(self.mainWindow,  height=20, width=60, spacing1=10, spacing2=2, wrap='word', padx=40)
-        self.textBox.pack(expand=True, fill='both')
-        self.prjDescription = []
-        self.chapterTitles = []
-        self.chapterDescriptions = []
-        self.sceneTitles = []
-        self.sceneDescriptions = []
-        self.sceneContents = []
+        self._textBox = RichTextTk(self._mainWindow,  height=20, width=60, spacing1=10, spacing2=2, wrap='word', padx=40)
+        self._textBox.pack(expand=True, fill='both')
+        self._prjDescription = []
+        self._chapterTitles = []
+        self._chapterDescriptions = []
+        self._sceneTitles = []
+        self._sceneDescriptions = []
+        self._sceneContents = []
 
-    def extend_menu(self):
+    def _extend_menu(self):
         """Add main menu entries.
         Override the superclass template method. 
         """
-        self.quickViewMenu = tk.Menu(self.mainMenu, title='my title', tearoff=0)
-        self.mainMenu.add_cascade(label='Quick view', menu=self.quickViewMenu)
-        self.mainMenu.entryconfig('Quick view', state='disabled')
-        self.quickViewMenu.add_command(label='Project description', command=lambda: self.show_text(self.prjDescription))
-        self.quickViewMenu.add_command(label='Chapter titles', command=lambda: self.show_text(self.chapterTitles))
-        self.quickViewMenu.add_command(label='Chapter descriptions',
-                                       command=lambda: self.show_text(self.chapterDescriptions))
-        self.quickViewMenu.add_command(label='Scene titles', command=lambda: self.show_text(self.sceneTitles))
-        self.quickViewMenu.add_command(label='Scene descriptions',
-                                       command=lambda: self.show_text(self.sceneDescriptions))
-        self.quickViewMenu.add_command(label='Scene contents', command=lambda: self.show_text(self.sceneContents))
-        self.quickViewMenu.insert_separator(1)
-        self.quickViewMenu.insert_separator(4)
+        self._quickViewMenu = tk.Menu(self._mainMenu, title='my title', tearoff=0)
+        self._mainMenu.add_cascade(label='Quick view', menu=self._quickViewMenu)
+        self._mainMenu.entryconfig('Quick view', state='disabled')
+        self._quickViewMenu.add_command(label='Project description', command=lambda: self._show_text(self._prjDescription))
+        self._quickViewMenu.add_command(label='Chapter titles', command=lambda: self._show_text(self._chapterTitles))
+        self._quickViewMenu.add_command(label='Chapter descriptions',
+                                       command=lambda: self._show_text(self._chapterDescriptions))
+        self._quickViewMenu.add_command(label='Scene titles', command=lambda: self._show_text(self._sceneTitles))
+        self._quickViewMenu.add_command(label='Scene descriptions',
+                                       command=lambda: self._show_text(self._sceneDescriptions))
+        self._quickViewMenu.add_command(label='Scene contents', command=lambda: self._show_text(self._sceneContents))
+        self._quickViewMenu.insert_separator(1)
+        self._quickViewMenu.insert_separator(4)
 
-    def disable_menu(self):
+    def _disable_menu(self):
         """Disable menu entries when no project is open.
         Extend the superclass method.      
         """
-        super().disable_menu()
-        self.mainMenu.entryconfig('Quick view', state='disabled')
+        super()._disable_menu()
+        self._mainMenu.entryconfig('Quick view', state='disabled')
 
-    def enable_menu(self):
+    def _enable_menu(self):
         """Enable menu entries when a project is open.
         Extend the superclass method.
         """
-        super().enable_menu()
-        self.mainMenu.entryconfig('Quick view', state='normal')
+        super()._enable_menu()
+        self._mainMenu.entryconfig('Quick view', state='normal')
 
-    def show_text(self, text):
+    def _show_text(self, text):
         """Load text into the text box.
         Disable text editing.
         """
-        self.textBox['state'] = 'normal'
-        self.textBox.delete('1.0', tk.END)
+        self._textBox['state'] = 'normal'
+        self._textBox.delete('1.0', tk.END)
 
         for paragraph in text:
-            self.textBox.insert(tk.END, paragraph[0], paragraph[1])
+            self._textBox.insert(tk.END, paragraph[0], paragraph[1])
 
-        self.textBox['state'] = 'disabled'
+        self._textBox['state'] = 'disabled'
 
     def open_project(self, fileName):
         """Create a yWriter project instance and read the file.
@@ -89,10 +89,10 @@ class Yw7ViewerTk(MainTk):
 
         statView (str): String containing the total numbers of chapters, scenes and words.
         descView: (list of tuples): Project description.
-        chapterTitles (list of tuples): List of chapter titles.
-        chapterDescriptions (list of tuples): Text containing chapter titles and descriptions.
-        sceneTitles (list of tuples): Text containing chapter titles and listed scene titles.
-        sceneContents (list of tuples): Text containing chapter titles and scene contents.
+        _chapterTitles (list of tuples): List of chapter titles.
+        _chapterDescriptions (list of tuples): Text containing chapter titles and descriptions.
+        _sceneTitles (list of tuples): Text containing chapter titles and listed scene titles.
+        _sceneContents (list of tuples): Text containing chapter titles and scene contents.
 
         (The list entries are tuples containing the text and a formatting tag.)       
         """
@@ -106,58 +106,58 @@ class Yw7ViewerTk(MainTk):
         if not fileName:
             return ''
 
-        self.ywPrj = Yw7File(fileName)
-        message = self.ywPrj.read()
+        self._ywPrj = Yw7File(fileName)
+        message = self._ywPrj.read()
 
         if message.startswith(ERROR):
-            self.close_project()
+            self._close_project()
             self.set_info_how(message)
             return ''
 
-        if self.ywPrj.title:
-            titleView = self.ywPrj.title
+        if self._ywPrj.title:
+            titleView = self._ywPrj.title
 
         else:
             titleView = 'Untitled yWriter project'
 
-        if self.ywPrj.author:
-            authorView = self.ywPrj.author
+        if self._ywPrj.author:
+            authorView = self._ywPrj.author
 
         else:
             authorView = 'Unknown author'
 
-        self.titleBar.config(text=f'{titleView} by {authorView}')
+        self._titleBar.config(text=f'{titleView} by {authorView}')
 
         # Get project description.
 
-        self.prjDescription = []
+        self._prjDescription = []
 
-        if self.ywPrj.desc:
-            self.prjDescription.append((self.ywPrj.desc, ''))
+        if self._ywPrj.desc:
+            self._prjDescription.append((self._ywPrj.desc, ''))
 
         else:
-            self.prjDescription.append(('(No project description available)', 'italic'))
+            self._prjDescription.append(('(No project description available)', 'italic'))
 
-        self.chapterTitles = []
-        self.chapterDescriptions = []
-        self.sceneTitles = []
-        self.sceneDescriptions = []
-        self.sceneContents = []
+        self._chapterTitles = []
+        self._chapterDescriptions = []
+        self._sceneTitles = []
+        self._sceneDescriptions = []
+        self._sceneContents = []
         chapterCount = 0
         sceneCount = 0
         wordCount = 0
 
-        for chId in self.ywPrj.srtChapters:
+        for chId in self._ywPrj.srtChapters:
 
-            if self.ywPrj.chapters[chId].isUnused:
+            if self._ywPrj.chapters[chId].isUnused:
                 continue
 
-            if self.ywPrj.chapters[chId].chType != 0 and self.ywPrj.chapters[chId].oldType != 0:
+            if self._ywPrj.chapters[chId].chType != 0 and self._ywPrj.chapters[chId].oldType != 0:
                 continue
 
             chapterCount += 1
 
-            if self.ywPrj.chapters[chId].chLevel == 0:
+            if self._ywPrj.chapters[chId].chLevel == 0:
                 headingTag = RichTextTk.H2_TAG
                 listTag = ''
 
@@ -167,72 +167,72 @@ class Yw7ViewerTk(MainTk):
 
             # Get chapter titles.
 
-            if self.ywPrj.chapters[chId].title:
-                self.chapterTitles.append((f'{self.ywPrj.chapters[chId].title}\n', listTag))
-                sceneHeading = (f'{self.ywPrj.chapters[chId].title}\n', headingTag)
-                self.sceneTitles.append(sceneHeading)
+            if self._ywPrj.chapters[chId].title:
+                self._chapterTitles.append((f'{self._ywPrj.chapters[chId].title}\n', listTag))
+                sceneHeading = (f'{self._ywPrj.chapters[chId].title}\n', headingTag)
+                self._sceneTitles.append(sceneHeading)
 
             # Get chapter descriptions.
 
-            if self.ywPrj.chapters[chId].desc:
-                self.chapterDescriptions.append((f'{self.ywPrj.chapters[chId].title}\n', headingTag))
-                self.chapterDescriptions.append((f'{self.ywPrj.chapters[chId].desc}\n', ''))
+            if self._ywPrj.chapters[chId].desc:
+                self._chapterDescriptions.append((f'{self._ywPrj.chapters[chId].title}\n', headingTag))
+                self._chapterDescriptions.append((f'{self._ywPrj.chapters[chId].desc}\n', ''))
 
-            for scId in self.ywPrj.chapters[chId].srtScenes:
+            for scId in self._ywPrj.chapters[chId].srtScenes:
 
-                if not (self.ywPrj.scenes[scId].isUnused or self.ywPrj.scenes[scId].isNotesScene or self.ywPrj.scenes[scId].isTodoScene):
+                if not (self._ywPrj.scenes[scId].isUnused or self._ywPrj.scenes[scId].isNotesScene or self._ywPrj.scenes[scId].isTodoScene):
                     sceneCount += 1
 
                     # Get scene titles.
 
-                    if self.ywPrj.scenes[scId].title:
-                        self.sceneTitles.append((f'{self.ywPrj.scenes[scId].title}\n', ''))
+                    if self._ywPrj.scenes[scId].title:
+                        self._sceneTitles.append((f'{self._ywPrj.scenes[scId].title}\n', ''))
 
                     # Get scene descriptions.
 
-                    if self.ywPrj.scenes[scId].desc:
-                        self.sceneDescriptions.append(sceneHeading)
-                        self.sceneDescriptions.append((f'{self.ywPrj.scenes[scId].desc}\n', ''))
+                    if self._ywPrj.scenes[scId].desc:
+                        self._sceneDescriptions.append(sceneHeading)
+                        self._sceneDescriptions.append((f'{self._ywPrj.scenes[scId].desc}\n', ''))
 
                     # Get scene contents.
 
-                    if self.ywPrj.scenes[scId].sceneContent:
-                        self.sceneContents.append(sceneHeading)
-                        self.sceneContents.append((convert_from_yw(f'{self.ywPrj.scenes[scId].sceneContent}\n'), ''))
+                    if self._ywPrj.scenes[scId].sceneContent:
+                        self._sceneContents.append(sceneHeading)
+                        self._sceneContents.append((convert_from_yw(f'{self._ywPrj.scenes[scId].sceneContent}\n'), ''))
 
                     sceneHeading = ('* * *\n', RichTextTk.CENTER_TAG)
 
                     # Get scene word count.
 
-                    if self.ywPrj.scenes[scId].wordCount:
-                        wordCount += self.ywPrj.scenes[scId].wordCount
+                    if self._ywPrj.scenes[scId].wordCount:
+                        wordCount += self._ywPrj.scenes[scId].wordCount
 
         self.statView = f'{chapterCount} chapters, {sceneCount} scenes, {wordCount} words'
 
-        if len(self.chapterTitles) == 0:
-            self.chapterTitles.append(('(No chapter titles available)', RichTextTk.ITALIC_TAG))
+        if len(self._chapterTitles) == 0:
+            self._chapterTitles.append(('(No chapter titles available)', RichTextTk.ITALIC_TAG))
 
-        if len(self.chapterDescriptions) == 0:
-            self.chapterDescriptions.append(('(No chapter descriptions available)', RichTextTk.ITALIC_TAG))
+        if len(self._chapterDescriptions) == 0:
+            self._chapterDescriptions.append(('(No chapter descriptions available)', RichTextTk.ITALIC_TAG))
 
-        if len(self.sceneTitles) == 0:
-            self.sceneTitles.append(('(No scene titles available)', RichTextTk.ITALIC_TAG))
+        if len(self._sceneTitles) == 0:
+            self._sceneTitles.append(('(No scene titles available)', RichTextTk.ITALIC_TAG))
 
-        if len(self.sceneDescriptions) == 0:
-            self.sceneDescriptions.append(('(No scene descriptions available)', RichTextTk.ITALIC_TAG))
+        if len(self._sceneDescriptions) == 0:
+            self._sceneDescriptions.append(('(No scene descriptions available)', RichTextTk.ITALIC_TAG))
 
-        if len(self.sceneContents) == 0:
-            self.sceneContents.append(('(No scene contents available)', RichTextTk.ITALIC_TAG))
+        if len(self._sceneContents) == 0:
+            self._sceneContents.append(('(No scene contents available)', RichTextTk.ITALIC_TAG))
 
-        self.show_text(self.prjDescription)
-        self.set_status(self.statView)
-        self.enable_menu()
+        self._show_text(self._prjDescription)
+        self._set_status(self.statView)
+        self._enable_menu()
 
-    def close_project(self):
+    def _close_project(self):
         """Clear the text box.
         Extend the superclass method.
         """
-        super().close_project()
-        self.textBox['state'] = 'normal'
-        self.textBox.delete('1.0', tk.END)
-        self.textBox['state'] = 'disabled'
+        super()._close_project()
+        self._textBox['state'] = 'normal'
+        self._textBox.delete('1.0', tk.END)
+        self._textBox['state'] = 'disabled'
