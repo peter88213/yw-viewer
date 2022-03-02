159fa43984b1628e95ebe8f4d4ce4e53091e60c6
peter88213 <peter.88213@t-online.de>
2022-03-02 11:23:48 +0100

    Refactor/reformat the code
    - Remove blank lines.
    - Update docstrings.
    - Change list checks.

--- src/yw_viewer_.pyw
+++ src/yw_viewer_.pyw
@@ -11,23 +11,19 @@ import os
 import sys
 import argparse
 from pathlib import Path
-
 from pywriter.config.configuration import Configuration
 from ywviewerlib.yw7_viewer_tk import Yw7ViewerTk
 
 APPNAME = 'yw-viewer'
-
 SETTINGS = dict(
     yw_last_open='',
 )
-
 OPTIONS = {}
 
 
 def run(sourcePath='', installDir=''):
 
     #--- Load configuration.
-
     iniFile = f'{installDir}{APPNAME}.ini'
     configuration = Configuration(SETTINGS, OPTIONS)
     configuration.read(iniFile)
@@ -36,26 +32,20 @@ def run(sourcePath='', installDir=''):
     kwargs.update(configuration.options)
 
     #--- Get initial project path.
-
     if not sourcePath or not os.path.isfile(sourcePath):
         sourcePath = kwargs['yw_last_open']
 
     #--- Instantiate the viewer object.
-
     viewer = Yw7ViewerTk('yw-viewer @release', **kwargs)
     viewer.open_project(sourcePath)
     viewer.start()
 
     #--- Save project specific configuration
-
     for keyword in viewer.kwargs:
-
         if keyword in configuration.options:
             configuration.options[keyword] = viewer.kwargs[keyword]
-
         elif keyword in configuration.settings:
             configuration.settings[keyword] = viewer.kwargs[keyword]
-
         configuration.write(iniFile)
 
 
@@ -64,15 +54,11 @@ if __name__ == '__main__':
     try:
         homeDir = str(Path.home()).replace('\\', '/')
         installDir = f'{homeDir}/.pywriter/{APPNAME}/config/'
-
     except:
         installDir = ''
-
     os.makedirs(installDir, exist_ok=True)
-
     if len(sys.argv) == 1:
         run('', installDir)
-
     else:
         parser = argparse.ArgumentParser(
             description='yWriter file viewer',
@@ -80,6 +66,5 @@ if __name__ == '__main__':
         parser.add_argument('sourcePath',
                             metavar='Sourcefile',
                             help='The path of the yWriter project file.')
-
         args = parser.parse_args()
         run(args.sourcePath, installDir)
--- src/ywviewerlib/yw7_viewer_tk.py
+++ src/ywviewerlib/yw7_viewer_tk.py
@@ -9,23 +9,32 @@ import os
 import re
 import tkinter as tk
 from tkinter import scrolledtext
-
 from pywriter.pywriter_globals import ERROR
 from pywriter.yw.yw7_file import Yw7File
 from pywriter.ui.main_tk import MainTk
-
 from ywviewerlib.rich_text_tk import RichTextTk
 
 
 class Yw7ViewerTk(MainTk):
     """A tkinter GUI class for yWriter file viewing.
+    
+    Public methods:
+        open_project(fileName, fileTypes=[('yWriter 7 project', '.yw7')]) 
+            -- select a valid project file and display the path.
 
     Show titles, descriptions, and contents in a text box.
     """
 
     def __init__(self, title, **kwargs):
         """Put a text box to the GUI main window.
-        Extend the superclass constructor.
+        
+        Positional arguments:
+            title -- application title to be displayed at the window frame.
+         
+        Required keyword arguments:
+            yw_last_open -- str: initial file.
+        
+        Extends the superclass constructor.
         """
         super().__init__(title, **kwargs)
         self._textBox = RichTextTk(self._mainWindow,  height=20, width=60, spacing1=10, spacing2=2, wrap='word', padx=40)
@@ -39,7 +48,8 @@ class Yw7ViewerTk(MainTk):
 
     def _extend_menu(self):
         """Add main menu entries.
-        Override the superclass template method. 
+        
+        Overrides the superclass template method. 
         """
         self._quickViewMenu = tk.Menu(self._mainMenu, title='my title', tearoff=0)
         self._mainMenu.add_cascade(label='Quick view', menu=self._quickViewMenu)
@@ -57,59 +67,63 @@ class Yw7ViewerTk(MainTk):
 
     def _disable_menu(self):
         """Disable menu entries when no project is open.
-        Extend the superclass method.      
+        
+        Extends the superclass method.      
         """
         super()._disable_menu()
         self._mainMenu.entryconfig('Quick view', state='disabled')
 
     def _enable_menu(self):
         """Enable menu entries when a project is open.
-        Extend the superclass method.
+        
+        Extends the superclass method.
         """
         super()._enable_menu()
         self._mainMenu.entryconfig('Quick view', state='normal')
 
-    def _show_text(self, tagged_text):
+    def _show_text(self, taggedText):
         """Load text into the text box.
+        
+        Positional arguments:
+            taggedText -- list of (text, formatting tag) tuples. 
+        
         Disable text editing.
         """
         self._textBox['state'] = 'normal'
         self._textBox.delete('1.0', tk.END)
-
-        for text, tag in tagged_text:
+        for text, tag in taggedText:
             self._textBox.insert(tk.END, text, tag)
-
         self._textBox['state'] = 'disabled'
 
     def open_project(self, fileName):
-        """Create a yWriter project instance and read the file.
+        """Select a valid project file and display the path.
+
+        Positional arguments:
+            fileName -- str: project file path.
+            
         Display project title, description and status.
-        Extend the superclass method.
-
         Create for quick viewing: 
-
-        statView (str): String containing the total numbers of chapters, scenes and words.
-        descView: (list of tuples): Project description.
-        _chapterTitles (list of tuples): List of chapter titles.
-        _chapterDescriptions (list of tuples): Text containing chapter titles and descriptions.
-        _sceneTitles (list of tuples): Text containing chapter titles and listed scene titles.
-        _sceneContents (list of tuples): Text containing chapter titles and scene contents.
-
+            statView -- str: String containing the total numbers of chapters, scenes and words.
+            descView -- list of tuples: Project description.
+            _chapterTitles -- list of tuples: List of chapter titles.
+            _chapterDescriptions -- list of tuples: Text containing chapter titles and descriptions.
+            _sceneTitles -- list of tuples: Text containing chapter titles and listed scene titles.
+            _sceneContents -- list of tuples: Text containing chapter titles and scene contents.
         (The list entries are tuples containing the text and a formatting tag.)       
+        
+        Extends the superclass method.
         """
 
         def convert_from_yw(text):
-            """Remove yw7 markup."""
+            """Remove yw7 markup from text."""
             return re.sub('\[\/*[i|b|h|c|r|s|u]\d*\]', '', text)
 
         fileName = super().open_project(fileName)
-
         if not fileName:
             return ''
 
         self._ywPrj = Yw7File(fileName)
         message = self._ywPrj.read()
-
         if message.startswith(ERROR):
             self._close_project()
             self.set_info_how(message)
@@ -117,28 +131,20 @@ class Yw7ViewerTk(MainTk):
 
         if self._ywPrj.title:
             titleView = self._ywPrj.title
-
         else:
             titleView = 'Untitled yWriter project'
-
         if self._ywPrj.authorName:
             authorView = self._ywPrj.authorName
-
         else:
             authorView = 'Unknown author'
-
         self._titleBar.config(text=f'{titleView} by {authorView}')
 
         # Get project description.
-
         self._prjDescription = []
-
         if self._ywPrj.desc:
             self._prjDescription.append((self._ywPrj.desc, ''))
-
         else:
             self._prjDescription.append(('(No project description available)', 'italic'))
-
         self._chapterTitles = []
         self._chapterDescriptions = []
         self._sceneTitles = []
@@ -147,9 +153,7 @@ class Yw7ViewerTk(MainTk):
         chapterCount = 0
         sceneCount = 0
         wordCount = 0
-
         for chId in self._ywPrj.srtChapters:
-
             if self._ywPrj.chapters[chId].isUnused:
                 continue
 
@@ -157,81 +161,65 @@ class Yw7ViewerTk(MainTk):
                 continue
 
             chapterCount += 1
-
             if self._ywPrj.chapters[chId].chLevel == 0:
                 headingTag = RichTextTk.H2_TAG
                 listTag = ''
-
             else:
                 headingTag = RichTextTk.H1_TAG
                 listTag = RichTextTk.BOLD_TAG
 
             # Get chapter titles.
-
             if self._ywPrj.chapters[chId].title:
                 self._chapterTitles.append((f'{self._ywPrj.chapters[chId].title}\n', listTag))
                 sceneHeading = (f'{self._ywPrj.chapters[chId].title}\n', headingTag)
                 self._sceneTitles.append(sceneHeading)
-
+            
             # Get chapter descriptions.
-
             if self._ywPrj.chapters[chId].desc:
                 self._chapterDescriptions.append((f'{self._ywPrj.chapters[chId].title}\n', headingTag))
                 self._chapterDescriptions.append((f'{self._ywPrj.chapters[chId].desc}\n', ''))
 
             for scId in self._ywPrj.chapters[chId].srtScenes:
-
                 if not (self._ywPrj.scenes[scId].isUnused or self._ywPrj.scenes[scId].isNotesScene or self._ywPrj.scenes[scId].isTodoScene):
                     sceneCount += 1
 
                     # Get scene titles.
-
                     if self._ywPrj.scenes[scId].title:
                         self._sceneTitles.append((f'{self._ywPrj.scenes[scId].title}\n', ''))
 
                     # Get scene descriptions.
-
                     if self._ywPrj.scenes[scId].desc:
                         self._sceneDescriptions.append(sceneHeading)
                         self._sceneDescriptions.append((f'{self._ywPrj.scenes[scId].desc}\n', ''))
 
                     # Get scene contents.
-
                     if self._ywPrj.scenes[scId].sceneContent:
                         self._sceneContents.append(sceneHeading)
                         self._sceneContents.append((convert_from_yw(f'{self._ywPrj.scenes[scId].sceneContent}\n'), ''))
-
                     sceneHeading = ('* * *\n', RichTextTk.CENTER_TAG)
 
                     # Get scene word count.
-
                     if self._ywPrj.scenes[scId].wordCount:
                         wordCount += self._ywPrj.scenes[scId].wordCount
-
         statView = f'{chapterCount} chapters, {sceneCount} scenes, {wordCount} words'
-
-        if len(self._chapterTitles) == 0:
+        if not self._chapterTitles:
             self._chapterTitles.append(('(No chapter titles available)', RichTextTk.ITALIC_TAG))
-
-        if len(self._chapterDescriptions) == 0:
+        if not self._chapterDescriptions:
             self._chapterDescriptions.append(('(No chapter descriptions available)', RichTextTk.ITALIC_TAG))
-
-        if len(self._sceneTitles) == 0:
+        if not self._sceneTitles:
             self._sceneTitles.append(('(No scene titles available)', RichTextTk.ITALIC_TAG))
-
-        if len(self._sceneDescriptions) == 0:
+        if not self._sceneDescriptions:
             self._sceneDescriptions.append(('(No scene descriptions available)', RichTextTk.ITALIC_TAG))
-
-        if len(self._sceneContents) == 0:
+        if not self._sceneContents:
             self._sceneContents.append(('(No scene contents available)', RichTextTk.ITALIC_TAG))
-
         self._show_text(self._prjDescription)
         self._set_status(statView)
         self._enable_menu()
 
     def _close_project(self):
         """Clear the text box.
-        Extend the superclass method.
+        
+        Extends the superclass method.
         """
         super()._close_project()
         self._textBox['state'] = 'normal'
