937ba45545734db99563d9f78a6a1fa2e704389b
peter88213 <peter.88213@t-online.de>
2023-06-09 12:52:04 +0200

    Update docstring

--- tools/build_yw_viewer.py
+++ tools/build_yw_viewer.py
@@ -1,11 +1,9 @@
-""" Build a python script for the yw-viewer distribution.
+"""Build a Python script for the yw-viewer distribution.
         
 In order to distribute a single script without dependencies, 
 this script "inlines" all modules imported from the pywriter package.
 
-The PyWriter project (see see https://github.com/peter88213/PyWriter)
-must be located on the same directory level as the yw-viewer project. 
-
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/yw-viewer
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
